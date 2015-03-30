6568d34edd73c2104c7ff3512d8edbbc9bae735f
ihsanudin <surya.kejawen@gmail.com>
2015-03-30 10:53:28 +0700

    fix missing filter on UserController

--- Security/UserController.php
+++ Security/UserController.php
@@ -12,12 +12,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use Symfonian\Indonesia\AdminBundle\Annotation\PageTitle;
 use Symfonian\Indonesia\AdminBundle\Annotation\PageDescription;
+use Symfonian\Indonesia\AdminBundle\Annotation\FilterFields;
 
 /**
  * @Route("/user")
  *
  * @PageTitle("page.user.title")
  * @PageDescription("page.user.description")
+ * @FilterFields({"username", "fullName"})
  */
 class UserController extends CrudController
 {
