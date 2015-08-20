83a40301c37dd5855e8eba74b97306c1102fd38a
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-08-20 16:59:45 +0700

    fix typo

--- Compiler/DefaultTemplateRegister.php
+++ Compiler/DefaultTemplateRegister.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfonian\Indonesia\AdminBundle\Controller\CrudController;
 
-final class DefaultTamplateRegister
+final class DefaultTemplateRegister
 {
     /**
      * @var ContainerInterface
