90ad44190530039f5273358ba7df135fe7f47f00
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-08-26 15:04:40 +0700

    remove entity check

--- Annotation/Schema/Crud.php
+++ Annotation/Schema/Crud.php
@@ -102,10 +102,6 @@ class Crud
 
     public function setEntity($entity)
     {
-        if (!is_subclass_of($entity, 'Symfonian\Indonesia\CoreBundle\Toolkit\DoctrineManager\Model\EntityInterface')) {
-            throw new \InvalidArgumentException(sprintf('Entity %s must implement Symfonian\Indonesia\CoreBundle\Toolkit\DoctrineManager\Model\EntityInterface', $entity));
-        }
-
         $this->entity = $entity;
     }
 
