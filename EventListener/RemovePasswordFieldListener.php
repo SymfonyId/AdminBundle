54b7364baeeffbabf95cee2254f411c5c28a21f4
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-11-09 22:32:38 +0700

    update

--- EventListener/RemovePasswordFieldListener.php
+++ EventListener/RemovePasswordFieldListener.php
@@ -2,7 +2,29 @@
 
 namespace Symfonian\Indonesia\AdminBundle\EventListener;
 
+/*
+ * Author: Muhammad Surya Ihsanuddin<surya.kejawen@gmail.com>
+ * Url: https://github.com/ihsanudin
+ */
+
+use Symfonian\Indonesia\AdminBundle\Event\FilterResponseEvent;
+use Symfonian\Indonesia\AdminBundle\Security\Model\User;
+
 class RemovePasswordFieldListener
 {
+    public function onPreCreateForm(FilterResponseEvent $event)
+    {
+        $formData = $event->getFormData();
+        if (!$formData instanceof User) {
+            return;
+        }
 
+        /** @var \Symfony\Component\Form\FormInterface $form */
+        $form = $event->getForm();
+        if (!$formData->getId() || 'user' !== $form->getName()) {
+            return;
+        }
+
+        $form->remove('plainPassword');
+    }
 }
