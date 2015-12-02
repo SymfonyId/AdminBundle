847e7ad09c06eb12359158a0ff593691c9ed1b60
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-12-02 09:40:55 +0700

    update

--- Form/UserType.php
+++ Form/UserType.php
@@ -84,7 +84,7 @@ class UserType extends AbstractType
         $resolver->setDefaults(array(
             'data_class' => $this->container->getParameter('symfonian_id.admin.security.user_entity'),
             'translation_domain' => $this->container->getParameter('symfonian_id.admin.translation_domain'),
-            'validation_groups' => array('Registration', 'Default'),
+            'validation_groups' => array('Default'),
             'intention' => $this->getName(),
         ));
     }
