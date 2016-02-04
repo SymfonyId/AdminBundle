9139b5ead5cd7d4fa6ec6b37894552f2a6b08c8f
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-02-04 13:34:09 +0700

    improve configurator

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,8 +2,13 @@
 
 namespace Symfonian\Indonesia\AdminBundle\Controller;
 
+use Symfonian\Indonesia\AdminBundle\Annotation\Crud;
+use Symfonian\Indonesia\AdminBundle\Annotation\Grid;
+use Symfonian\Indonesia\AdminBundle\Annotation\Page;
+use Symfonian\Indonesia\AdminBundle\Annotation\Util;
 use Symfonian\Indonesia\AdminBundle\Configuration\Configurator;
 use Symfonian\Indonesia\AdminBundle\SymfonianIndonesiaAdminConstants as Constants;
+use Symfonian\Indonesia\CoreBundle\Toolkit\Util\ArrayUtil\ArrayNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as Base;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -27,9 +32,25 @@ class Controller extends Base
 
         $cacheDir = $this->container->getParameter('kernel.cache_dir');
         $caches = require $cacheDir.Constants::CACHE_PATH;
+        $configurations = $caches[__CLASS__];
+        foreach ($configurations as $key => $configuration) {
+            $config = null;
+            if (Crud::class === $key) {
+                $config = new Crud();
+            }
+            if (Grid::class === $key) {
+                $config = new Crud();
+            }
+            if (Page::class === $key) {
+                $config = new Crud();
+            }
+            if (Util::class === $key) {
+                $config = new Crud();
+            }
+            $this->configurator->addConfiguration(ArrayNormalizer::convertToObject($configuration, $config));
+        }
 
-        var_dump($caches);
-        exit();
+        $this->configurator->freeze();
     }
 
     private function isProduction()
