4b84ae946b6a62f1a570c4ef9f2e7767dec80fef
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-02-10 15:25:20 +0700

    init test

---
+++ tests/Configuration/ConfiguratorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Symfonian\Indonesia\AdminBundle\Tests\Configuration;
+
+/*
+ * Author: Muhammad Surya Ihsanuddin<surya.kejawen@gmail.com>
+ * Url: https://github.com/ihsanudin
+ */
+
+use Symfonian\Indonesia\AdminBundle\Annotation\Crud;
+use Symfonian\Indonesia\AdminBundle\Annotation\Grid;
+use Symfonian\Indonesia\AdminBundle\Annotation\Page;
+use Symfonian\Indonesia\AdminBundle\Annotation\Util;
+use Symfonian\Indonesia\AdminBundle\Configuration\Configurator;
+use Symfonian\Indonesia\AdminBundle\Extractor\ExtractorFactory;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpKernel\Kernel;
+
+class ConfiguratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Configurator
+     */
+    private $configurator;
+
+    public function setUp()
+    {
+        $kernel = $this->getMockBuilder(Kernel::class)->disableOriginalConstructor()->getMock();
+        $extractor = $this->getMockBuilder(ExtractorFactory::class)->disableOriginalConstructor()->getMock();
+        $formFactory = $this->getMockBuilder(FormFactory::class)->disableOriginalConstructor()->getMock();
+
+        $this->configurator = new Configurator($kernel, $extractor, $formFactory);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Can't change any configuration during production
+     */
+    public function testFreezeConfiguration()
+    {
+        $this->configurator->freeze();
+        $this->configurator->addConfiguration(new Crud());
+    }
+
+    public function testGetConfiguration()
+    {
+        $this->defaultConfiguration();
+
+        $this->assertEquals(Crud::class, get_class($this->configurator->getConfiguration(Crud::class)));
+        $this->assertEquals(Grid::class, get_class($this->configurator->getConfiguration(Grid::class)));
+        $this->assertEquals(Page::class, get_class($this->configurator->getConfiguration(Page::class)));
+        $this->assertEquals(Util::class, get_class($this->configurator->getConfiguration(Util::class)));
+    }
+
+    private function defaultConfiguration()
+    {
+        $this->configurator->addConfiguration(new Crud());
+        $this->configurator->addConfiguration(new Grid());
+        $this->configurator->addConfiguration(new Page());
+        $this->configurator->addConfiguration(new Util());
+    }
+}
