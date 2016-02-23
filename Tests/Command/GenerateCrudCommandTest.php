a706a800c741f0a0ee874550da4505bf7cbe61ce
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-02-24 03:33:43 +0700

    improve test

--- Tests/Command/GenerateCrudCommandTest.php
+++ Tests/Command/GenerateCrudCommandTest.php
@@ -27,13 +27,42 @@ class GenerateCrudCommandTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $container = $this->getMockBuilder(ContainerInterface::class)->disableOriginalConstructor()->setMethods(array('set', 'get', 'has', 'initialized', 'setParameter', 'getParameter', 'hasParameter'))->getMock();
-        $container->expects($this->any())->method('get')->with('filesystem')->willReturn($this->getMockBuilder(Filesystem::class)->disableOriginalConstructor()->getMock());
-        $this->command = $this->getMockBuilder(GenerateCrudCommand::class)->disableOriginalConstructor()->setMethods(array('setContainer', 'getContainer', 'getSkeletonDirs'))->getMock();
-        $this->command->expects($this->any())->method('getSkeletonDirs')->willReturn(null);
-        $this->command->expects($this->any())->method('getControllerGenerator')->willReturn(null);
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array(
+                'set',
+                'get',
+                'has',
+                'initialized',
+                'setParameter',
+                'getParameter',
+                'hasParameter',
+            ))
+            ->getMock()
+        ;
+        $container
+            ->expects($this->any())
+            ->method('get')
+            ->with('filesystem')
+            ->willReturn(
+                $this->getMockBuilder(Filesystem::class)
+                ->disableOriginalConstructor()
+                ->getMock()
+            )
+        ;
+        $this->command = $this->getMockBuilder(GenerateCrudCommand::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array(
+                'setContainer',
+                'getContainer',
+                'getSkeletonDirs',
+                'getHelperSet',
+                'configure',
+            ))
+            ->getMock()
+        ;
         $this->command->expects($this->any())->method('getContainer')->willReturn($container);
-        $this->command->expects($this->any())->method('createGenerator')->willReturn(null);
+        $this->command->expects($this->any())->method('execute')->withAnyParameters()->willReturn(null);
     }
 
     public function testProtectedMethodsMustSuccess()
@@ -41,5 +70,6 @@ class GenerateCrudCommandTest extends \PHPUnit_Framework_TestCase
         $this->invokeMethod($this->command, 'getControllerGenerator');
         $this->invokeMethod($this->command, 'getSkeletonDirs');
         $this->invokeMethod($this->command, 'createGenerator');
+        $this->invokeMethod($this->command, 'configure');
     }
 }
