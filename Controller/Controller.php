3f2c01927d43e7497d823cb914b2d29821be1e5b
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-02-25 09:59:32 +0700

    improve controller to prevent missing cache

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -41,7 +41,12 @@ abstract class Controller extends Base
 
         $cacheDir = $this->container->getParameter('kernel.cache_dir');
         $cacheFile = str_replace('\\', '_', $key);
-        $configurations = require sprintf('%s/%s/%s.php.cache', $cacheDir, Constants::CACHE_DIR, $cacheFile);
+        $fullPath = sprintf('%s/%s/%s.php.cache', $cacheDir, Constants::CACHE_DIR, $cacheFile);
+        if (!file_exists($fullPath)) {//It's impossible but we need to prevent and make sure it is not throwing an exception
+            return $configurator;
+        }
+
+        $configurations = require $fullPath;
         /** @var array $configuration */
         foreach ($configurations as $k => $configuration) {
             $config = null;
