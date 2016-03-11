71d53179c8b7ad43ad8bd2c931a0cd4dc5c4a766
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-03-11 18:09:56 +0700

    update listener

--- SymfonianIndonesiaAdminConstants.php
+++ SymfonianIndonesiaAdminConstants.php
@@ -19,6 +19,7 @@ class SymfonianIndonesiaAdminConstants
     const PRE_FORM_CREATE = 'siab.pre_form_create';
     const PRE_FORM_SUBMIT = 'siab.pre_form_submit';
     const PRE_SAVE = 'siab.pre_save';
+    const POST_SAVE = 'siab.post_save';
     const FILTER_LIST = 'siab.filter_query';
     const PRE_DELETE = 'siab.pre_delete';
     const PRE_SHOW = 'siab.pre_show';
