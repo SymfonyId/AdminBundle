0ad85fb12e89cb3df82246b20e0f5694af9ee188
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-03-24 11:09:01 +0700

    change file author

--- Configuration/ConfiguratorAwareTrait.php
+++ Configuration/ConfiguratorAwareTrait.php
@@ -1,9 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ihsan
- * Date: 3/3/16
- * Time: 6:40 AM.
+
+/*
+ * This file is part of the AdminBundle package.
+ *
+ * (c) Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Symfonian\Indonesia\AdminBundle\Configuration;
